4ae68ff980800d18e5e7bba2ae654c95b3f61f89
Ky-oo <kylian.patry@gmail.com>
2025-10-08 13:10:56 +0200

    Create Navbar

---
+++ app/layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="mx-auto">
+    <nav class="py-4 bg-orange-500 px-4">
+      <NuxtLink to="/" class="font-bold px-4 text-lg">Eat It</NuxtLink>
+    </nav>
+    <main>
+      <slot />
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
